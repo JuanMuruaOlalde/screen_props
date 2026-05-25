443ad355126ad91e6ae8300d7aeb2d11285049ee
Juan Murua Olalde <juan@susosise.es>
2026-05-25 17:00:23 +0200

    correct a little mistake

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // eframe - hide console window on Windows in release
 #[macro_use]
 extern crate rust_i18n;
-i18n!("locales", fallback = "em");
+i18n!("locales", fallback = "en");
 
 use eframe::egui;
 
